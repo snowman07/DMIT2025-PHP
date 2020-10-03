a23f49ee7ff3403db5b2c58f3dbf67110bd33295
Arr Belrey Domingo <59627816+snowman07@users.noreply.github.com>
2020-10-03 11:28:01 -0600

    did some commenting

--- week04-FormValidation/03-samepage-pretty/indexMine.php
+++ week04-FormValidation/03-samepage-pretty/indexMine.php
@@ -79,12 +79,12 @@
     <div class="form-group">
         <label for="gender">Gender</label>
         <div class="form-check">
-        <input class="form-check-input" type="radio" name="gender" />
-        <label class="form-check-label" for="gender"> Male </label>
+          <input class="form-check-input" type="radio" name="gender" />
+          <label class="form-check-label" for="gender"> Male </label>
         </div>
         <div class="form-check">
-        <input class="form-check-input" type="radio" name="gender" />
-        <label class="form-check-label" for="gender"> Female </label>
+          <input class="form-check-input" type="radio" name="gender" />
+          <label class="form-check-label" for="gender"> Female </label>
         </div>
         <!--Male:
         <input
--- week04-FormValidation/03-samepage-pretty/login.php
+++ week04-FormValidation/03-samepage-pretty/login.php
@@ -1,15 +1,16 @@
 <?php 
 
-if(isset($_POST['mysubmit'])){
+	if(isset($_POST['mysubmit'])){
 
-	$user = trim($_POST['user']);
-	$email = trim($_POST['email']);
-	$province = trim($_POST['province']);
-	$gender = trim($_POST['gender']);
-	$newsletter = trim($_POST['newsletter']);
-	$comments = trim($_POST['comments']);
-}
-	
+		$username = trim($_POST['username']);
+		$email = trim($_POST['email']);
+		$province = trim($_POST['province']);
+		$gender = trim($_POST['gender']);
+		$newsletter = trim($_POST['newsletter']);
+		$comments = trim($_POST['comments']);
+
+		echo "$username, $email, $province, $gender, $newsletter, $comments";
+	}	
 ?>
 <!DOCTYPE html>
 <html>
@@ -22,8 +23,8 @@ if(isset($_POST['mysubmit'])){
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 
 	<style type="text/css">
-		.formstyle{ /* optional: in case you don't like the really wide form */
-			max-width:650px;
+		.container{ /* optional: in case you don't like the really wide form */
+			max-width: 650px;
 		}
 
 	</style>
@@ -32,82 +33,84 @@ if(isset($_POST['mysubmit'])){
 
 </head>
 <body>
+
 	<div class="container">
-
 		<h1>PHP Form Validation</h1>
-		<form name="myform" class="formstyle" method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
-
 		
-		<div class="form-group">
-		    <label for="user">Name:</label>
-		    <input type="text" class="form-control" name="user" value="">  
-
-		  
-
-		  </div>
-
-		  <div class="form-group">
-		    <label for="email">Email:</label>
-		    <input type="text" class="form-control" name="email" value="">  
-		    
-		  </div>
-
-		  <div class="form-group">
-		    <label for="province">Province:</label>
-		   	<select name="province" class="form-control">
-				<option value="AB">Alberta</option>
-				<option value="BC">British Columbia</option>
-				<option value="MB">Manitoba</option>
-				<option value="NB">New Brunswick</option>
-				<option value="NL">Newfoundland and Labrador</option>
-				<option value="NS">Nova Scotia</option>
-				<option value="ON">Ontario</option>
-				<option value="PE">Prince Edward Island</option>
-				<option value="QC">Quebec</option>
-				<option value="SK">Saskatchewan</option>
-				<option value="NT">Northwest Territories</option>
-				<option value="NU">Nunavut</option>
-				<option value="YT">Yukon</option>
-			</select>
+		<!--start of form-->
+		<form name="myform" class="formstyle" method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
 			
-		  </div>
-
-		  <br>
-
-		  <div class="form-check">
-			  <label class="form-check-label">
-			    <input type="radio" class="form-check-input" name="gender" value="male">Male
-			  </label>
-			</div>
-			<div class="form-check">
-			  <label class="form-check-label">
-			    <input type="radio" class="form-check-input" name="gender" value="female" >Female
-			  </label>
-			</div>
-
-			<br>
-
-			<div class="form-check">
-			  <label class="form-check-label">
-			    <input type="checkbox" class="form-check-input" name="newsletter" value="1" >Subscribe to Newsletter
-			  </label>
-			</div>
-
-			<br>
-
-
+			<!--start of Name-->
 			<div class="form-group">
-			    <label for="comments">Comments:</label>
-			   	<textarea name="comments" class="form-control" rows="4"></textarea>  
-			  </div>
-
-		   <input type="submit" class="btn btn-primary" name="mysubmit">
+				<label for="username">Name:</label>
+				<input type="text" class="form-control" name="username" placeholder="Enter name here" value="">  
+			</div>
+			<!--end of Name-->
+			<br>
+			<!--start of Email-->
+			<div class="form-group">
+				<label for="email">Email:</label>
+				<input type="text" class="form-control" name="email" placeholder="Enter email here" value=""> 	
+			</div>
+			<!--end of Email-->
+			<br>
+			<!--start of Province-->
+			<div class="form-group">
+				<label for="province">Province:</label>
+				<select name="province" class="form-control">
+					<option value="">---Select province---</option>
+					<option value="AB">Alberta</option>
+					<option value="BC">British Columbia</option>
+					<option value="MB">Manitoba</option>
+					<option value="NB">New Brunswick</option>
+					<option value="NL">Newfoundland and Labrador</option>
+					<option value="NS">Nova Scotia</option>
+					<option value="ON">Ontario</option>
+					<option value="PE">Prince Edward Island</option>
+					<option value="QC">Quebec</option>
+					<option value="SK">Saskatchewan</option>
+					<option value="NT">Northwest Territories</option>
+					<option value="NU">Nunavut</option>
+					<option value="YT">Yukon</option>
+				</select>
+			</div>
+			<!--end of Province-->
+			<br>
+			<!--start of Gender-->
+			<div class="form-group">
+				<label for="gender">Gender</label>
+				<div class="form-check">
+					<label class="form-check-label">
+						<input type="radio" class="form-check-input" name="gender" value="male">Male
+					</label>
+				</div>
+				<div class="form-check">
+					<label class="form-check-label">
+						<input type="radio" class="form-check-input" name="gender" value="female" >Female
+					</label>
+				</div>
+			</div>
+			<!--end of Gender-->
+			<br>
+			<!--start of Subscribe-->
+			<div class="form-check">
+				<label class="form-check-label">
+					<input type="checkbox" class="form-check-input" name="newsletter" value="1" >Subscribe to Newsletter
+				</label>
+			</div>
+			<!--end of Subscribe-->
+			<br>
+			<!--start of Comments-->
+			<div class="form-group">
+				<label for="comments">Comments:</label>
+				<textarea name="comments" class="form-control" rows="4"></textarea>  
+			</div>
+			<!--end of Comments-->
+			<br><br>
+			<input type="submit" class="btn btn-primary" name="mysubmit">
 
 		</form>
-
-		
-
-
+		<!--end of form-->
 	</div><!-- / .container -->
 
 </body>
