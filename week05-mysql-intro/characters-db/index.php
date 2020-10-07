08a46c21bd8543dbf4b6e24be7ac76db45c811fc
snowman07 <59627816+snowman07@users.noreply.github.com>
2020-10-07 11:45:49 -0600

    need to review oct 7 wednesday recording

---
+++ week05-mysql-intro/characters-db/index.php
@@ -0,0 +1,30 @@
+<?php
+
+include ("includes/header.php");
+
+?>
+
+  <div class="jumbotron clearfix">
+        <h1><?php echo APP_NAME ?></h1>
+        <p class="lead">This is a simple Bootstrap template to assist in creating simple applications in PHP. <br>
+        	Please make the necessary changes as you need.
+        </p>
+        <a class="btn btn-primary float-right" href="#" role="button">Button</a>
+      </div>
+
+
+      
+   <p> Bonbon sweet roll caramels sesame snaps. Gingerbread danish tiramisu cotton candy marzipan. Chocolate cake biscuit topping bonbon sweet roll wafer toffee lemon drops. Caramels lollipop tart cake marshmallow ice cream. Gingerbread candy canes wafer carrot cake soufflé dragée jelly-o candy. Fruitcake pudding tootsie roll croissant. Tart sweet bonbon gingerbread jelly beans tootsie roll. Pudding chupa chups carrot cake danish cookie pastry jelly dessert. Pastry cake biscuit chupa chups dragée. Bear claw dragée ice cream. Sweet ice cream toffee topping jelly-o cupcake. Candy canes soufflé lemon drops muffin sesame snaps. Sugar plum sweet roll sugar plum apple pie. Oat cake jelly marshmallow gummi bears croissant fruitcake.</p>
+
+<p>Croissant soufflé biscuit biscuit marshmallow. Liquorice sweet caramels topping. Oat cake icing cheesecake. Gummi bears soufflé tootsie roll ice cream tart chupa chups gummi bears. Apple pie toffee oat cake jelly cookie candy canes chocolate bar gummi bears. Toffee cotton candy jelly-o tiramisu. Soufflé sugar plum gummi bears tootsie roll jujubes. Biscuit muffin soufflé icing chocolate bar. Sweet biscuit cookie powder chocolate tootsie roll. Marshmallow chupa chups pastry gummi bears chocolate cake jelly beans. Croissant gingerbread toffee croissant caramels oat cake. Sugar plum cotton candy gingerbread danish gingerbread cotton candy jujubes cotton candy. Bear claw dessert tootsie roll.</p>
+
+<p>Marshmallow jelly beans croissant gingerbread cotton candy toffee lemon drops lollipop powder. Icing jelly lollipop icing candy canes candy. Oat cake dragée cupcake chocolate. Dessert candy canes cookie cake. Ice cream lemon drops wafer brownie. Cupcake cheesecake bonbon soufflé tootsie roll sweet roll bonbon. Jelly-o caramels tiramisu gummies chupa chups cotton candy bonbon. Cheesecake croissant bear claw. Wafer bear claw pie. Sesame snaps soufflé candy sweet tootsie roll. Brownie marzipan tootsie roll. Cake candy candy canes toffee cake cupcake dragée lemon drops sugar plum.</p>
+
+<p>Topping lollipop toffee tart powder tiramisu lemon drops tootsie roll. Jelly-o carrot cake cookie cotton candy bear claw danish. Sugar plum chocolate cake cotton candy. Cake marshmallow jelly cupcake bonbon powder. Dessert gummies jelly-o halvah candy canes liquorice sesame snaps biscuit. Cake danish soufflé jelly-o. Gingerbread gummi bears fruitcake cake jelly beans pie icing. Lemon drops tart halvah gingerbread cheesecake chocolate bar muffin icing wafer. Tiramisu cookie sugar plum chupa chups apple pie gummies apple pie wafer. Toffee biscuit lemon drops cotton candy brownie caramels cheesecake caramels. Biscuit marzipan icing sugar plum sugar plum cotton candy croissant. Tootsie roll brownie gingerbread croissant liquorice. Tart biscuit soufflé wafer dragée.</p>
+
+
+
+<?php
+
+include ("includes/footer.php");
+?>
