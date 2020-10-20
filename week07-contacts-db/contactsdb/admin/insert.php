fe38b859c4cb1b02a6f3d74c77cd7f8adf25178f
snowman07 <59627816+snowman07@users.noreply.github.com>
2020-10-19 23:45:06 -0600

    in prog insert page

--- week07-contacts-db/contactsdb/admin/insert.php
+++ week07-contacts-db/contactsdb/admin/insert.php
@@ -1,24 +1,58 @@
 <?php
 	include("../includes/header.php");
+
+	//$result = mysqli_query($con, "SELECT * FROM characters"); //this is only for list.php
+
+	$business_name = trim($_POST["business-name"]);
+	$last_name = trim($_POST["last-name"]);
+	$description = trim($_POST["description"]);
+
+	//echo $first_name . " " . $last_name . " " . $age . " " . $occupation . " " . $description;
+	
+
+
 ?>
 <h2>Insert</h2>
-<form id="myform" name="myform" method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
-		<div class="form-group">
-			<label for="alpha">Alpha:</label>
-			<input type="text" name="alpha" class="form-control">
-		</div>
-		<div class="form-group">
-			<label for="beta">Beta:</label>
-			<textarea name="beta" class="form-control"></textarea>
-		</div>
-		<div class="form-group">
-			<label for="submit">&nbsp;</label>
-			<input type="submit" name="submit" class="btn btn-info" value="Submit">
-		</div>
 
+<form id="myform" name="myform" method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
+	<div class="form-group">
+		<label for="alpha">Alpha:</label>
+		<input type="text" name="alpha" class="form-control">
+	</div>
+	<div class="form-group">
+		<label for="beta">Beta:</label>
+		<textarea name="beta" class="form-control"></textarea>
+	</div>
+	<div class="form-group">
+		<label for="submit">&nbsp;</label>
+		<input type="submit" name="submit" class="btn btn-info" value="Submit">
+	</div>
+
+	<!--start of Business Name-->
+	<div class="form-group">
+		<label for="business-name">Business Name:</label>
+		<input
+			type="text"
+			class="form-control"
+			name="business-name"
+			placeholder="Enter business name here"
+			value="<?php echo $business_name; // prepopulate the value type text input?>"
+		>
+		<!-- <?php
+			if($valFirstNameMsg) { echo $msgPreError. $valFirstNameMsg. $msgPost; } // this is validation
+		?> -->
+	</div>
+	<!--end of Business Name-->
+
+	<div>&nbsp;</div>
+	<!-- space before button -->
+	<button type="submit" name="mysubmit" class="btn btn-primary mb-2">
+		Submit
+	</button>
 
 
 </form>
+
 <?php
 	include("../includes/footer.php");
 ?>
