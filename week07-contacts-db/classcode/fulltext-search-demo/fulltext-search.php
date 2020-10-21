6dc1745c2ce6ce8b30d8bc0e09edff0368776fff
snowman07 <59627816+snowman07@users.noreply.github.com>
2020-10-21 11:45:04 -0600

    full text search demo discussion

---
+++ week07-contacts-db/classcode/fulltext-search-demo/fulltext-search.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+		
+		<input type="text" name="searchterm">
+		<input type="submit" name="mysubmit">
+	</form>
+	<?php 
+	/*
+
+You may have to do this to index a certain column (in SQL tab): ALTER TABLE table_name ADD FULLTEXT(column_name);
+Table storage format Must be MYISAM: Can access this in phpMyAdmin under Operations > Table Options
+
+*/
+
+	if(isset($_POST['mysubmit'])){
+		
+		//echo $searchterm;
+		$con = mysqli_connect("localhost", "adomingo4","PDsbNesMr3sSS79","adomingo4_dmit2025");
+
+		$searchterm = mysqli_real_escape_string($con, $_POST['searchterm']); 
+		// add more columns here so you can search them
+		$sql = "SELECT * FROM cd_catalog_class WHERE MATCH (title,description) AGAINST ('$searchterm' IN BOOLEAN MODE)";
+
+
+		echo "search results for <b>$searchterm</b>";
+		$result = mysqli_query($con,$sql) or die (mysqli_error($con));
+			
+		while ($row = mysqli_fetch_array( $result )){
+			
+			$title = $row['title'];
+			
+	
+			
+			echo "<p>". $title . "</p>";
+			}
+		
+
+	}
+
+
+
+
+
+	 ?>
+</body>
+</html>
