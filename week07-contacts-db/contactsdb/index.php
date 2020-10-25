8d4dab3e650a3e937213b83627fcea7a07a0cc1c
snowman07 <59627816+snowman07@users.noreply.github.com>
2020-10-25 07:26:04 -0600

    index.php DONE

--- week07-contacts-db/contactsdb/index.php
+++ week07-contacts-db/contactsdb/index.php
@@ -1,27 +1,33 @@
 <?php
-
-// Connect to DB: Since all files depend on this, this will be included in our header, which is then included in all files.
-$con = mysqli_connect("localhost", "adomingo4","PDsbNesMr3sSS79","adomingo4_dmit2025");
-
-include ("includes/header.php");
-
+  include ("includes/header.php");
 ?>
-
   
-
+<div class="jumbotron clearfix">
+  <h1><?php echo APP_NAME ?></h1>
+  <p class="lead">
+    This is a simple MySQL CRUDS application of potential employers and business contacts. This application will demonstrate the MySQL queries behind a widespread web usability feature: A listing of items from where the user can select a single item to view in detail:
+    <ul class="lead">
+      <li>List view (aka thumbnail view if an image is present): A minimum of information shown for many items.</li>
+      <li>Single Item View: A whole page and all information available for one item only.</li>
+    </ul>
+  </p>
+  <p class="lead">
+    Also, a technique in PHP known as alternate syntax is introduced. This is a different way of writing PHP structures that allows for more PHP blocks, but less actual PHP within an HTML document. 
+  </p>
+  <!-- <a class="btn btn-primary float-right" href="#" role="button">Button</a> -->
+</div>
 
 <?php
-    // Here, lets retrieve and list all our characters
-    $result = mysqli_query($con, "SELECT * FROM arr_contacts ORDER BY arr_bizname ASC");    
+  // Here, lets retrieve and list all our characters
+  $result = mysqli_query($con, "SELECT * FROM arr_contacts ORDER BY arr_bizname ASC");    
 ?>
 
-<?php while($row = mysqli_fetch_array($result)): ?> 
-<!-- above is a ternary operator with a colon ":" -->
-
+<?php while($row = mysqli_fetch_array($result)): ?> <!-- ternary operator with a colon ":" -->
   <!-- ALL of this is simple HTML, then I uses PHP "mixins" to grab the data-->
   <div class="alert alert-info">
-    <p><?php echo $row["arr_bizname"]; ?><a href="companyprofile.php?id=<?php echo $row['id'] ?> ">  View Company... </a></p> <!--companyprofile.php?id= is a query string-->
-    <!-- <p><?php echo $row["arr_name"]; ?></p>
+    <p class="lead"><?php echo $row["arr_bizname"]; ?><a href="companyprofile.php?id=<?php echo $row['id'] ?> "> View details here </a></p> <!--companyprofile.php?id= is a query string-->
+    <!-- 
+    <p><?php echo $row["arr_name"]; ?></p>
     <p><?php echo $row["arr_email"]; ?></p>
     <p><?php echo $row["arr_website"]; ?></p>
     <p><?php echo $row["arr_phone"]; ?></p>
@@ -29,39 +35,11 @@ include ("includes/header.php");
     <p><?php echo $row["arr_city"]; ?></p>
     <p><?php echo $row["arr_prov"]; ?></p>
     <p><?php echo $row["arr_desc"]; ?></p>
-    <p><?php echo $row["arr_resume"]; ?></p> -->
+    <p><?php echo $row["arr_resume"]; ?></p> 
+    -->
   </div>
-
-
-<!-- to end while loop-->
-<?php endwhile; ?>
-
-
-
-  
-  
-<!--   
-  <div class="jumbotron clearfix">
-        <h1><?php echo APP_NAME ?></h1>
-        <p class="lead">This is a simple Bootstrap template to assist in creating simple applications in PHP. <br>
-        	Please make the necessary changes as you need.
-        </p>
-        <a class="btn btn-primary float-right" href="#" role="button">Button</a>
-      </div>
-
-
-      
-   <p> Bonbon sweet roll caramels sesame snaps. Gingerbread danish tiramisu cotton candy marzipan. Chocolate cake biscuit topping bonbon sweet roll wafer toffee lemon drops. Caramels lollipop tart cake marshmallow ice cream. Gingerbread candy canes wafer carrot cake soufflé dragée jelly-o candy. Fruitcake pudding tootsie roll croissant. Tart sweet bonbon gingerbread jelly beans tootsie roll. Pudding chupa chups carrot cake danish cookie pastry jelly dessert. Pastry cake biscuit chupa chups dragée. Bear claw dragée ice cream. Sweet ice cream toffee topping jelly-o cupcake. Candy canes soufflé lemon drops muffin sesame snaps. Sugar plum sweet roll sugar plum apple pie. Oat cake jelly marshmallow gummi bears croissant fruitcake.</p>
-
-<p>Croissant soufflé biscuit biscuit marshmallow. Liquorice sweet caramels topping. Oat cake icing cheesecake. Gummi bears soufflé tootsie roll ice cream tart chupa chups gummi bears. Apple pie toffee oat cake jelly cookie candy canes chocolate bar gummi bears. Toffee cotton candy jelly-o tiramisu. Soufflé sugar plum gummi bears tootsie roll jujubes. Biscuit muffin soufflé icing chocolate bar. Sweet biscuit cookie powder chocolate tootsie roll. Marshmallow chupa chups pastry gummi bears chocolate cake jelly beans. Croissant gingerbread toffee croissant caramels oat cake. Sugar plum cotton candy gingerbread danish gingerbread cotton candy jujubes cotton candy. Bear claw dessert tootsie roll.</p>
-
-<p>Marshmallow jelly beans croissant gingerbread cotton candy toffee lemon drops lollipop powder. Icing jelly lollipop icing candy canes candy. Oat cake dragée cupcake chocolate. Dessert candy canes cookie cake. Ice cream lemon drops wafer brownie. Cupcake cheesecake bonbon soufflé tootsie roll sweet roll bonbon. Jelly-o caramels tiramisu gummies chupa chups cotton candy bonbon. Cheesecake croissant bear claw. Wafer bear claw pie. Sesame snaps soufflé candy sweet tootsie roll. Brownie marzipan tootsie roll. Cake candy candy canes toffee cake cupcake dragée lemon drops sugar plum.</p>
-
-<p>Topping lollipop toffee tart powder tiramisu lemon drops tootsie roll. Jelly-o carrot cake cookie cotton candy bear claw danish. Sugar plum chocolate cake cotton candy. Cake marshmallow jelly cupcake bonbon powder. Dessert gummies jelly-o halvah candy canes liquorice sesame snaps biscuit. Cake danish soufflé jelly-o. Gingerbread gummi bears fruitcake cake jelly beans pie icing. Lemon drops tart halvah gingerbread cheesecake chocolate bar muffin icing wafer. Tiramisu cookie sugar plum chupa chups apple pie gummies apple pie wafer. Toffee biscuit lemon drops cotton candy brownie caramels cheesecake caramels. Biscuit marzipan icing sugar plum sugar plum cotton candy croissant. Tootsie roll brownie gingerbread croissant liquorice. Tart biscuit soufflé wafer dragée.</p>
- -->
-
+<?php endwhile; ?> <!-- to end while loop-->
 
 <?php
-
-include ("includes/footer.php");
+  include ("includes/footer.php");
 ?>
