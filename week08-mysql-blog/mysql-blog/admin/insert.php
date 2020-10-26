48a7f4f1ab0cb5f76ccb64e2faaa9e6fdce317b8
snowman07 <59627816+snowman07@users.noreply.github.com>
2020-10-26 11:17:26 -0600

    DONE: insert page. Added emoticons folder

--- week08-mysql-blog/mysql-blog/admin/insert.php
+++ week08-mysql-blog/mysql-blog/admin/insert.php
@@ -1,24 +1,109 @@
 <?php
 	include("../includes/header.php");
+
+	$title = trim($_POST["title"]);
+	$message = trim($_POST["message"]);
+
+	// if statement if the button has been pressed. Test that too!
+	if(isset($_POST["mysubmit"])) {
+		
+		// VALIDATION HERE!!!
+
+		// lets set some variables for later use
+		$valid = 1;	//assume everyting is OK, go ahead and process form data
+			// vars here are for bootstrap design
+		$msgPreError = "\n<div class=\"alert alert-danger\" role=\"alert\">";
+		$msgPreSuccess = "\n<div class=\"alert alert-primary\" role=\"alert\">";
+		$msgPost = "\n</div>";
+
+		//Title validation
+		if((strlen($title) < 1) || (strlen($title) > 20)) {
+			$valid = 0;
+			$valTitleMsg = "\nPlease enter title from 1 to 20 characters";
+		}
+		//Title validation
+
+		//Message validation
+		if($message != "") {
+			if((strlen($message) < 3) || (strlen($message) > 100)) {
+				$valid = 0;
+				$valMessageMsg = "Message must be 3 to 100 characters";
+			}
+		}
+
+		// if((strlen($description) < 3) || (strlen($description) > 1000)) {
+		// 	$valid = 0;
+		// 	$valDescriptionMsg = "Description must be 3 to 1000 characters";
+		// }
+		//END of Message validation
+
+		//SUCCESS!!! If boolean ($valid) is still 1, then user form data is good, go ahead and process this form
+		if($valid == 1) {
+
+			// mysql INSERT
+			mysqli_query($con, "INSERT INTO arr_blog(arr_title, arr_message) 
+								VALUES('$title', '$message')") 
+								or die(mysqli_error($con));
+
+			$msgSuccess = "New blog inserted.";
+
+			// IF SUCCESS, form will be blank
+			$title = "";
+			$message = "";
+		}
+	} // END of mysubmit
 ?>
-<h2>Insert</h2>
+
+<div class="jumbotron clearfix">
+  <h1>Insert Blog</h1>
+  <p class="lead">
+    You can insert blog here.
+  </p>
+  <a class="btn btn-primary float-right" href="logout.php" role="button">Logout</a>
+</div>
+
+
 <form id="myform" name="myform" method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
-		<div class="form-group">
-			<label for="alpha">Alpha:</label>
-			<input type="text" name="alpha" class="form-control">
-		</div>
-		<div class="form-group">
-			<label for="beta">Beta:</label>
-			<textarea name="beta" class="form-control"></textarea>
-		</div>
-		<div class="form-group">
-			<label for="submit">&nbsp;</label>
-			<input type="submit" name="submit" class="btn btn-info" value="Submit">
-		</div>
+		
+	<?php
+		if($msgSuccess) {
+			echo $msgPreSuccess.$msgSuccess.$msgPost;
+		}
+	?>
+		
+	<!--start of Title-->
+	<div class="form-group">
+		<label for="title">Title:</label>
+		<input
+			type="text"
+			class="form-control"
+			name="title"
+			placeholder="Enter title name here"
+			value="<?php echo $title; // prepopulate the value type text input?>"
+		>
+		<?php
+			if($valTitleMsg) { echo $msgPreError. $valTitleMsg. $msgPost; } // this is validation
+		?>
+	</div>
+	<!--end of Title-->
 
+	<!--start of Message-->
+	<div class="form-group">
+		<label for="message">Message:</label>
+		<textarea class="form-control" name="message" rows="3"><?php if($message) {echo $message;} ?></textarea>
+		<?php
+			if($valMessageMsg) { echo $msgPreError. $valMessageMsg. $msgPost; }
+		?>
+	</div>
+	<!--end of Message-->
 
-
+	<!-- Submit button -->
+	<button type="submit" name="mysubmit" class="btn btn-primary mb-2">
+		Submit
+	</button>
+	<!-- End of Submit button -->
 </form>
+
 <?php
 	include("../includes/footer.php");
 ?>
--- week08-mysql-blog/mysql-blog/index.php
+++ week08-mysql-blog/mysql-blog/index.php
@@ -1,30 +1,50 @@
 <?php
-
-include ("includes/header.php");
-
+  include ("includes/header.php");
 ?>
 
-  <div class="jumbotron clearfix">
-        <h1><?php echo APP_NAME ?></h1>
-        <p class="lead">This is a simple Bootstrap template to assist in creating simple applications in PHP. <br>
-        	Please make the necessary changes as you need.
-        </p>
-        <a class="btn btn-primary float-right" href="#" role="button">Button</a>
-      </div>
-
-
-      
-   <p> Bonbon sweet roll caramels sesame snaps. Gingerbread danish tiramisu cotton candy marzipan. Chocolate cake biscuit topping bonbon sweet roll wafer toffee lemon drops. Caramels lollipop tart cake marshmallow ice cream. Gingerbread candy canes wafer carrot cake soufflé dragée jelly-o candy. Fruitcake pudding tootsie roll croissant. Tart sweet bonbon gingerbread jelly beans tootsie roll. Pudding chupa chups carrot cake danish cookie pastry jelly dessert. Pastry cake biscuit chupa chups dragée. Bear claw dragée ice cream. Sweet ice cream toffee topping jelly-o cupcake. Candy canes soufflé lemon drops muffin sesame snaps. Sugar plum sweet roll sugar plum apple pie. Oat cake jelly marshmallow gummi bears croissant fruitcake.</p>
-
-<p>Croissant soufflé biscuit biscuit marshmallow. Liquorice sweet caramels topping. Oat cake icing cheesecake. Gummi bears soufflé tootsie roll ice cream tart chupa chups gummi bears. Apple pie toffee oat cake jelly cookie candy canes chocolate bar gummi bears. Toffee cotton candy jelly-o tiramisu. Soufflé sugar plum gummi bears tootsie roll jujubes. Biscuit muffin soufflé icing chocolate bar. Sweet biscuit cookie powder chocolate tootsie roll. Marshmallow chupa chups pastry gummi bears chocolate cake jelly beans. Croissant gingerbread toffee croissant caramels oat cake. Sugar plum cotton candy gingerbread danish gingerbread cotton candy jujubes cotton candy. Bear claw dessert tootsie roll.</p>
-
-<p>Marshmallow jelly beans croissant gingerbread cotton candy toffee lemon drops lollipop powder. Icing jelly lollipop icing candy canes candy. Oat cake dragée cupcake chocolate. Dessert candy canes cookie cake. Ice cream lemon drops wafer brownie. Cupcake cheesecake bonbon soufflé tootsie roll sweet roll bonbon. Jelly-o caramels tiramisu gummies chupa chups cotton candy bonbon. Cheesecake croissant bear claw. Wafer bear claw pie. Sesame snaps soufflé candy sweet tootsie roll. Brownie marzipan tootsie roll. Cake candy candy canes toffee cake cupcake dragée lemon drops sugar plum.</p>
-
-<p>Topping lollipop toffee tart powder tiramisu lemon drops tootsie roll. Jelly-o carrot cake cookie cotton candy bear claw danish. Sugar plum chocolate cake cotton candy. Cake marshmallow jelly cupcake bonbon powder. Dessert gummies jelly-o halvah candy canes liquorice sesame snaps biscuit. Cake danish soufflé jelly-o. Gingerbread gummi bears fruitcake cake jelly beans pie icing. Lemon drops tart halvah gingerbread cheesecake chocolate bar muffin icing wafer. Tiramisu cookie sugar plum chupa chups apple pie gummies apple pie wafer. Toffee biscuit lemon drops cotton candy brownie caramels cheesecake caramels. Biscuit marzipan icing sugar plum sugar plum cotton candy croissant. Tootsie roll brownie gingerbread croissant liquorice. Tart biscuit soufflé wafer dragée.</p>
-
+<div class="jumbotron clearfix">
+  <h1><?php echo APP_NAME ?></h1>
+  <p class="lead">This is a simple Bootstrap template to assist in creating simple applications in PHP. <br>
+    Please make the necessary changes as you need.
+  </p>
+  <a class="btn btn-primary float-right" href="#" role="button">Button</a>
+</div>
 
 
 <?php
+  // Here, lets retrieve and list all our characters
+  $result = mysqli_query($con, "SELECT * FROM arr_blog ORDER BY bid DESC");    
+?>
 
-include ("includes/footer.php");
+<?php while($row = mysqli_fetch_array($result)): ?> <!-- ternary operator with a colon ":" -->
+  <!-- ALL of this is simple HTML, then I uses PHP "mixins" to grab the data-->
+  <div class="alert alert-info clearfix">
+    <p class="lead float-right">
+      <?php 
+        $thisDate = strtotime($row["arr_timedate"]);// retrieve data from DB
+        $thisDate = date("F j, Y g:i a",$thisDate); // modify using date() function
+        //Please edit the format you want and write it to the browser how you like.
+        // output $thisDate later in your markup
+        //echo "<b>". $row["arr_timedate"] ."</b>"; 
+        echo "<b>". $thisDate ."</b>"; 
+      ?><br>
+    </p>
+    <p class="lead">
+      <?php 
+        echo "<b>". $row["arr_title"] ."</b>"; 
+      ?><br>
+      <?php 
+        echo "<b>". $row["arr_message"] ."</b>"; 
+      ?><br>
+      
+    </p> <!--companyprofile.php?id= is a query string-->
+    <!-- 
+    <p><?php echo $row["arr_title"]; ?></p>
+    -->
+  </div>
+<?php endwhile; ?> <!-- to end while loop-->
+
+
+<?php
+  include ("includes/footer.php");
 ?>
