eea8b71e4bb2d5e2e18efeee186e065b286be6af
snowman07 <59627816+snowman07@users.noreply.github.com>
2020-10-25 19:55:34 -0600

    DONE: fulltext search

--- week07-contacts-db/contactsdb/fulltext-search.php
+++ week07-contacts-db/contactsdb/fulltext-search.php
@@ -9,7 +9,7 @@
   </p>
   <!-- <a class="btn btn-primary float-right" href="#" role="button">Button</a> -->
 </div>
-<h2>Type business name:</h2>
+<h2>Full-text search:</h2>
 <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>"> 
     <input type="text" name="searchterm">
     <input type="submit" name="mysubmit" value="Search">
@@ -27,7 +27,7 @@
 
 		$searchterm = mysqli_real_escape_string($con, $_POST['searchterm']); 
 		// add more columns here so you can search them
-		$sql = "SELECT * FROM arr_contacts WHERE MATCH (arr_bizname) AGAINST ('$searchterm' IN BOOLEAN MODE)"; //fulltext searching
+		$sql = "SELECT * FROM arr_contacts WHERE MATCH (arr_bizname, arr_desc) AGAINST ('$searchterm' IN BOOLEAN MODE)"; //fulltext searching
 
         echo "<br>";
         echo "Search results for: <b>$searchterm</b>";
@@ -35,25 +35,30 @@
 		$result = mysqli_query($con,$sql) or die (mysqli_error($con));
             
         if (mysqli_num_rows($result) > 0) {
-            while ($row = mysqli_fetch_array( $result )){
+          while ($row = mysqli_fetch_array( $result )){
+              
+            $business_name = $row['arr_bizname'];
+            //$your_name = $row['arr_name'];
+            // $artist = $row['artist'];
+            $description = $row['arr_desc'];
                 
-                    $business_name = $row['arr_bizname'];
-                    //$your_name = $row['arr_name'];
-                    // $artist = $row['artist'];
-                    // $description = $row['description'];
-                    
-            
-                echo "<div class=\"alert alert-info\">"; 
-                    echo "<p>". $business_name . "</p>";
-                    //echo "<p>". $your_name . "</p>";
-                    // echo "<p>". $artist . "</p>";
-                    // echo "<p>". $description . "</p><br>";
-                echo "</div>";
+            echo "<div class=\"alert alert-info\">"; 
+              echo "<br><p><b>Business Name: </b>". $business_name . "</p>";
+              // echo "<p>". $your_name . "</p>";
+              // echo "<p>". $email . "</p>";
+              // echo "<p>". $website . "</p><br>";
+              // echo "<p>". $phone_number . "</p><br>";
+              // echo "<p>". $address . "</p><br>";
+              // echo "<p>". $city . "</p><br>";
+              // echo "<p>". $province . "</p><br>";
+              echo "<p><b>Description: </b>". $description . "</p><br>";
+              //echo "<p>". $newsletter . "</p><br>";
+            echo "</div>";
 
-            } // end of while
+          } // end of while
 
         } else {
-            echo "\n<div class=\"alert alert-warning\">No results</div>\n";
+          echo "\n<div class=\"alert alert-warning\">No results</div>\n";
         }
 	}
 ?>
