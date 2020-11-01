1c740ea4eae8e041bba75a72e9d037de53e7e060
snowman07 <59627816+snowman07@users.noreply.github.com>
2020-11-01 11:49:34 -0700

    TODO: prepop the select

--- week08-mysql-blog/mysql-blog/admin/edit.php
+++ week08-mysql-blog/mysql-blog/admin/edit.php
@@ -1,4 +1,18 @@
-<!-- add session_start here! -->
+<!-- this is a secured page -->
+<?php
+    session_start();
+    //echo session_id();
+
+    //if(!isset($_SESSION['aasdffrtgfb'])) {        //<-- from here     isset means something is set
+    if(isset($_SESSION['aasdffrtgfbqw'])) {                //<-- to here
+        //header("Location: login.php");
+        //echo "Logged in";
+    } else {
+        //echo "NOT logged in";
+        header("Location: login.php");
+    }
+?>
+
 <?php
     include("../includes/header.php");
 
