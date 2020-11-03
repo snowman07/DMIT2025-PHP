cfed7740e3793696bc6e514f0455af8331b31bed
snowman07 <59627816+snowman07@users.noreply.github.com>
2020-11-02 20:23:16 -0700

    recording done reviewing

--- week09-image-methods-uploads/php-images/09-photofxemboss.php
+++ week09-image-methods-uploads/php-images/09-photofxemboss.php
@@ -6,6 +6,10 @@ $emboss = array(array(2,0,0), array(0,-1,0), array(0,0,-1));
 
 imageconvolution($im, $emboss, 1, 127);
 
-header()
+header("Content-type:image/jpeg");
+
+imagejpeg($im);
+
+imagedestroy($im);
 
 ?>
