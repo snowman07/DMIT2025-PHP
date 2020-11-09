e6ab5d3492db7b9e6f324a26ae7dc6b9ded998f6
snowman07 <59627816+snowman07@users.noreply.github.com>
2020-11-09 11:15:51 -0700

    modify mysql_connect.php to align lab7 requirements

--- week10-image-gallery-application/lab7-image-gallery/includes/mysql_connect.php
+++ week10-image-gallery-application/lab7-image-gallery/includes/mysql_connect.php
@@ -21,12 +21,12 @@ if (mysqli_connect_errno()) {
 // 	We use a constant (basically the same as a variable (stores a value)) to set the Base URL. We will use this to resolve all links as absolute URL's instead of relative URL's. If we do this correct, then we will not have any troubles pathing to all our files.
   // Make sure the path starts with http:// and ends with the trailing slash
   // If you copy/paste this script into other projects and fail to change this, then you may have links going back to the former project. Copy/Paste/FAIL !
-  define("BASE_URL", "http://adomingo4.dmitstudent.ca/dmit2025/week08/mysql-blog/");
+  define("BASE_URL", "http://adomingo4.dmitstudent.ca/dmit2025/week10/lab7-image-gallery/");
   
 
 
 
 
   // Your App Name
-  define("APP_NAME", "MySQL Blog");
+  define("APP_NAME", "Image Gallery");
 ?>
