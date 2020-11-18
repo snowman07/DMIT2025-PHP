8c7915abd378987a5e29f11df735360015ac1d23
snowman07 <59627816+snowman07@users.noreply.github.com>
2020-11-18 12:07:49 -0700

    class exercise on filtering DB

--- week11-filtering-db/filter_db_dogs_class/widgets.php
+++ week11-filtering-db/filter_db_dogs_class/widgets.php
@@ -57,7 +57,31 @@ while($row = mysqli_fetch_array($randomDog)) {
 // from http://www.webhostingtalk.com/showthread.php?t=717692
 // user "bigfan"
 
+
+
+
 echo "<h3>Alphabetical List</h3>";
+
+
+/*Mysql Left Function is used to return the leftmost string character from the string.
+Column Alias: 
+http://www.geeksengine.com/database/basic-select/column-alias.php
+*/
+$qry = "SELECT *, LEFT(breed, 1) AS first_char FROM dogs 
+        WHERE UPPER(breed) BETWEEN 'A' AND 'Z'
+        ORDER BY breed";
+$result = mysqli_query($con,$qry);
+$current_char = '';
+while ($row = mysqli_fetch_assoc($result)) {
+    if ($row['first_char'] != $current_char) {
+        $current_char = $row['first_char'];
+        echo '<br /><b>' . strtoupper($current_char) . '</b><br />';
+    }
+    //echo $row['breed'] . '<br />';
+	$breed = $row['breed'];
+	$pooch_id = $row['pooch_id'];
+	echo "$breed <a href=\"breed.php?pooch_id=$pooch_id\">More info...</a><br />";
+}
 
 /*Mysql Left Function is used to return the leftmost string character from the string.
 Column Alias: 
@@ -70,6 +94,19 @@ http://www.geeksengine.com/database/basic-select/column-alias.php
 ////////////////////////////////////////// ALPHABETICAL A - Z LINKS
 echo "<h3>Alphabetical A - Z Links only</h3>";
 
+
+$qry = "SELECT *, LEFT(breed, 1) AS first_char FROM dogs 
+        WHERE UPPER(breed) BETWEEN 'A' AND 'Z'
+        ORDER BY breed";
+$result = mysqli_query($con,$qry);
+$current_char = '';
+while ($row = mysqli_fetch_assoc($result)) {
+    if ($row['first_char'] != $current_char) {
+        $current_char = $row['first_char'];
+        $thisChar = strtoupper($current_char);
+        echo "<a href=\"index.php?displayby=breed&displayvalue=$thisChar%\">$thisChar</a> | ";
+    }
+}
 
 
 ?>
