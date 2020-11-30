f9633e166fcd2ef66218a3123da6c08704f0f650
snowman07 <59627816+snowman07@users.noreply.github.com>
2020-11-30 16:55:14 -0700

    insert still IN PROG

--- week12-catalog-project/admin/insert.php
+++ week12-catalog-project/admin/insert.php
@@ -22,10 +22,14 @@
     $thumbsFolder = "../uploads/thumbs/"; ////////// PATH MODIFICATION
     $displayFolder = "../uploads/display/"; ////////// PATH MODIFICATION
 
-    $title = trim($_POST["title"]);
-    $description = trim($_POST["description"]);
+    $plant_name = trim($_POST["plant-name"]);
+    $plant_description = trim($_POST["plant-description"]);
     $filename = $_FILES["myfile"]["name"];
     $imageFileType = strtolower(pathinfo($filename, PATHINFO_EXTENSION));//Enable PNG uploads as well as JPG
+
+    $plant_price = trim($_POST["plant-price"]);
+    $plant_size = trim($_POST["plant-size"]);
+    $plant_type = trim($_POST["plant-type"]);
 
     if(isset($_POST["mysubmit"])) {
         /* Lets take a look at the $_FILES array. All the info about this file is in that  
@@ -43,7 +47,7 @@
         // echo "Errors: " . $_FILES["myfile"]["error"] . "<br>"; // any errors in uploading
         // echo "Tmp name: " . $_FILES["myfile"]["tmp_name"] . "<br>"; // tmp name
 
-        // VALIDATION
+        //------------- VALIDATION-------------------//
         $valid = 1; // assumes validation has passed. Any validator can override this and set to 0
             // vars here for bootstrap design
         $msgPreError = "\n<div class=\"alert alert-danger\" role=\"alert\">";
@@ -53,21 +57,21 @@
         //Enable PNG uploads as well as JPG
         $allowed_extensions = array("image/png", "image/jpeg");
 
-        //Title validation
-		if((strlen($title) < 1) || (strlen($title) > 100)) {
+        //Plant Name validation
+		if((strlen($plant_name) < 1) || (strlen($plant_name) > 100)) {
 			$valid = 0;
-			$valTitleMsg = "\nPlease enter title from 1 to 100 characters";
+			$valPlantNameMsg = "\nPlease enter plant name from 1 to 100 characters";
 		}
-        //end of Title validation
+        //end of Plant Name validation
         
-        //Description validation
-        if($description != "") {
-			if((strlen($description) < 3) || (strlen($description) > 1000)) {
+        //Plant Description validation
+        if($plant_description != "") {
+			if((strlen($plant_description) < 3) || (strlen($plant_description) > 1000)) {
 				$valid = 0;
-				$valDescriptionMsg = "Message must be 3 to 1000 characters";
+				$valPlantDescriptionMsg = "Description must be 3 to 1000 characters";
 			}
 		}
-        //end of Description validation
+        //end of Plant Description validation
 
         //PNG and JPEG uploads VALIDATION
         if ($imageFileType != "png" && $imageFileType != "jpg" && $imageFileType != "jpeg") {
@@ -94,6 +98,28 @@
             $valid = 0;
             $valMessage = "You've not chosen a file.";
         }
+        
+
+        //Plant Price Validation
+        if($plant_price == "") {
+            $valid = 0;
+			$valPlantPriceMsg = "\nPlease enter price";
+        }
+        //END of Plant Price Validation
+
+        //Plant Size Validation
+        if($plant_size == "") {
+			$valid = 0;
+			$valPlantSizeMsg = "Please select a plant size";
+		}
+        //END of Plant Size Validation
+
+        //Plant Type Validation
+        if($plant_type == "") {
+			$valid = 0;
+			$valPlantTypeMsg = "Please select a plant type";
+		}
+        //END of Plant Type Validation
 
 
 
@@ -148,8 +174,8 @@
                     // $filename = $_FILES["myfile"]["name"];
 
                     // mysql INSERT
-                    mysqli_query($con, "INSERT INTO arr_lab7_image_gallery(arr_title, arr_description, arr_filename) 
-                                        VALUES('$title', '$description', '$filename')") 
+                    mysqli_query($con, "INSERT INTO plant_catalog(plant_name, plant_description, plant_price, plant_image, plant_size, plant_type) 
+                                        VALUES('$plant_name', '$plant_description', '$plant_price', '$filename', $plant_size, $plant_type)") 
                                         or die(mysqli_error($con));
 
                     $msgSuccess = "Upload successful.";
@@ -159,8 +185,8 @@
                     createThumbnailPNG($thisFile, $displayFolder, 800, $orientation);
 
                     // mysql INSERT
-                    mysqli_query($con, "INSERT INTO arr_lab7_image_gallery(arr_title, arr_description, arr_filename) 
-                                        VALUES('$title', '$description', '$filename')") 
+                    mysqli_query($con, "INSERT INTO plant_catalog(plant_name, plant_description, plant_price, plant_image, plant_size, plant_type) 
+                                        VALUES('$plant_name', '$plant_description', '$plant_price', '$filename', $plant_size, $plant_type)") 
                                         or die(mysqli_error($con));
 
                     $msgSuccess = "Upload successful.";
@@ -285,23 +311,23 @@ enctype="multipart/form-data"
             class="form-control"
             name="plant-name"
             placeholder="Enter plant name here"
-			value="<?php echo $title; // prepopulate the value type text input?>"
+			value="<?php echo $plant_name; // prepopulate the value type text input?>"
         >
         <?php
-			if($valTitleMsg) { echo $msgPreError. $valTitleMsg. $msgPost; } // this is validation
+			if($valPlantNameMsg) { echo $msgPreError. $valPlantNameMsg. $msgPost; } // this is validation
 		?>
     </div>
     <!--end of Plant Name-->
 
-    <!--start of Description-->
+    <!--start of Plant Description-->
     <div class="form-group">
-        <label for="description">Description</label>
-        <textarea class="form-control" name="description" rows="10"><?php if($description) {echo $description;} ?></textarea>
+        <label for="plant-description">Plant Description</label>
+        <textarea class="form-control" name="plant-description" rows="10"><?php if($plant_description) {echo $plant_description;} ?></textarea>
         <?php
-            if($valDescriptionMsg) { echo $msgPreError. $valDescriptionMsg. $msgPost; }
+            if($valPlantDescriptionMsg) { echo $msgPreError. $valPlantDescriptionMsg. $msgPost; }
         ?>
     </div>
-    <!--end of Description-->
+    <!--end of Plant Description-->
 
     <!--start of File-->
     <div class="form-group">
@@ -314,6 +340,61 @@ enctype="multipart/form-data"
         ?>
     </div>
     <!--end of File-->
+
+    <!--Plant Price-->
+    <div class="form-group">
+        <label for="plant-price">Plant Price</label>
+        <input 
+            type="number"
+            class="form-control"
+            name="plant-price"
+            placeholder="Enter plant price here"
+			value="<?php echo $plant_price; // prepopulate the value type text input?>"
+        >
+        <?php
+			if($valPlantPriceMsg) { echo $msgPreError. $valPlantPriceMsg. $msgPost; } // this is validation
+		?>
+    </div>
+    <!--Plant Price-->
+
+    <!--Plant Size-->
+    <div class="form-group">
+        <label for="plant-size">Plant Size</label>
+        <select class="form-control" name="plant-size">
+            <option value="">---Select size---</option>
+            <!--this serve as the placeholder-->
+            <option value="small" <?php if(isset($plant_size) && $plant_size == "small") {echo "selected";} ?>>Small</option>
+            <option value="medium" <?php if(isset($plant_size) && $plant_size == "medium") {echo "selected";} ?>>Medium</option>
+            <option value="large" <?php if(isset($plant_size) && $plant_size == "large") {echo "selected";} ?>>Large</option>
+            <!-- if(isset($province) && $province == "AB") {echo "selected";}  to prepop select options -->
+        </select>
+        <?php
+            if($valPlantSizeMsg) { echo $msgPreError. $valPlantSizeMsg. $msgPost; }
+        ?>
+    </div>
+    <!--END OF Plant Size-->
+
+    <!--Plant Type-->
+    <div class="form-group">
+        <label for="plant-type">Plant Type</label>
+        <select class="form-control" name="plant-type">
+            <option value="">---Select plant type---</option>
+            <!--this serve as the placeholder-->
+            <option value="common" <?php if(isset($plant_type) && $plant_type == "common") {echo "selected";} ?>>Common</option>
+            <option value="flowering" <?php if(isset($plant_type) && $plant_type == "flowering") {echo "selected";} ?>>Flowering</option>
+            <option value="lowlight" <?php if(isset($plant_type) && $plant_type == "lowlight") {echo "selected";} ?>>Lowlight</option>
+            <option value="cactus" <?php if(isset($plant_type) && $plant_type == "cactus") {echo "selected";} ?>>Cactus</option>
+            <!-- if(isset($province) && $province == "AB") {echo "selected";}  to prepop select options -->
+        </select>
+        <?php
+            if($valPlantTypeMsg) { echo $msgPreError. $valPlantTypeMsg. $msgPost; }
+        ?>
+    </div>
+    <!--END OF Plant Type-->
+
+
+
+
 
     <!--Submit button-->
     <button type="submit" name="mysubmit" class="btn btn-primary mb-2">
