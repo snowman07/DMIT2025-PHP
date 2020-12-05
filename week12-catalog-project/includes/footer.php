e477437349f23012a720ced43a5e6a987a128dc2
snowman07 <59627816+snowman07@users.noreply.github.com>
2020-12-04 20:49:21 -0700

    DONE footer

--- week12-catalog-project/includes/footer.php
+++ week12-catalog-project/includes/footer.php
@@ -7,6 +7,12 @@
 
     
   </body>
+
+  <footer style="background: #02325f; height: 100px;">
+    <h2 style="text-align: center; padding-top: 25px;">Live Green | Plant Green</h2>
+
+  </footer>
+
 </html>
 
 
