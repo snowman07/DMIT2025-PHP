347a012c132db10a52e3d768945dbefefd88bf54
snowman07 <59627816+snowman07@users.noreply.github.com>
2020-12-10 18:23:35 -0700

    remove php code for search bar

--- week12-catalog-project/includes/header.php
+++ week12-catalog-project/includes/header.php
@@ -75,43 +75,6 @@ include("mysql_connect.php");// here we include the connection script; since thi
           <button class="btn btn-outline-success my-2 my-sm-0" type="submit" name="searchsubmit">Search</button>
         </form>
         
-        
-        <!-- <?php 
-          $searchtext = trim($_POST["searchtext"]);
-          //include ("includes/search.php");
-          if(isset($_POST['searchsubmit']) && $searchtext != "") {
-            $sql = "SELECT * FROM plant_catalog WHERE
-                plant_name LIKE '$searchtext' ";
-
-            $result = mysqli_query($con, $sql);
-            // Lets deal with NO results from this query
-            if (mysqli_num_rows($result) > 0) {
-              // Now, we have to loop thru all records and display to the user
-              while($row = mysqli_fetch_array($result)) {
-                $plant_name = $row['plant_name'];
-                $plant_price = $row['plant_price'];
-                $plant_size = $row['plant_size'];
-                
-
-                
-                echo "<div class=\"square-img\">";
-                  echo "<center><b>" .$row["plant_name"] ."</center></b><br />\n";
-                  echo "<b>Price: $ </b>". $row["plant_price"] ."<br />\n";
-                  echo "<b>Size: </b>". $row["plant_size"] ."<br />\n";
-                echo "</div>";
-
-                // echo "<center><b>" .$row["plant_name"] ."</center></b><br />\n";
-                // echo "<b>Price: $ </b>". $row["plant_price"] ."<br />\n";
-                // echo "<b>Size: </b>". $row["plant_size"] ."<br />\n";
-                
-              } // end of while
-            } else {
-              echo "\n<div class=\"alert alert-warning\">No results</div>\n";
-            }
-          } // end of if 
-        ?> -->
-
-        
       </div>
       
     </nav>
